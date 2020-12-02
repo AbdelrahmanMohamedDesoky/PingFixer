acb76c7b11aa0f2fdb824c339d3cfd50c8baa15b
Abdelrahman Desoky <Abdelrahman.Desoky@Izam.co>
2020-12-02 10:34:15 +0200

    Added Host To Check To Env File

--- PingChecker.js
+++ PingChecker.js
@@ -2,7 +2,7 @@ const Ping = require('ping');
 const BAD_PING = 999;
 
 function getPing(){
-    return Ping.promise.probe('de.egyptsolution.com').catch(error => {
+    return Ping.promise.probe(process.env.HOST_TO_CHECK).catch(error => {
         console.error(error);
         return BAD_PING;
     });
